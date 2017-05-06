a0c7f6efa5acc5d42ae7a057f03a3ffa76ae588d
Lavamaster700 <Lavamaster700@users.noreply.github.com>
2017-05-05 21:39:06 -0500

    May5th
    Created a way to upload docket.

--- YouthAndGovernment.java
+++ YouthAndGovernment.java
@@ -4,8 +4,8 @@ public class YouthAndGovernment
 {
    public static void main(String[] args)
    {
-        final int OPENING_STATEMENT = 
-        int authorsSumnation = 180; //All times are shown in seconds
-        
+      System.out.println("Input the docket");
+      Docket docket = new Docket(scan.nextLine());
+      System.out.println(docket.toString());   
    }
 }
